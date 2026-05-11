6aae91b154d16f44984e53b7d3067e29605fed6e
Camilo Sasuke Thomas Borregaard Sørensen <camilosasuketbs@gmail.com>
2026-05-11 19:21:39 -0300

    🌌🌍 <3ThomasBSILoveYou!! 🌌🌍 [ macOS Tahoe 26.3.1 ] [ day 89 ] [ C++ ] ca2 Store and Stabilization - First time Metal later... <3ThomasBSJegElskerDig!!
    <3tbs, Mummi and bilbo!!
    
    Thomas Borregaard Sørensen \infinity,-0.16091989,\infinity ONE-MAN ABSOLUTE <3!! I love you, by ???-0.02041977-???write my history please make me please create me for you for me for you for me Camilo Sasuke Thomas Borregaard Sørensen!!
    
    Thomas 3 private commits on mid Dec2020!!
    
    Thomas Online YouTube VODs contribution!!
    
    Mummi 2021-11-19 01:01 (AM) Second Super Tuna Sandwich in Baguete with Lots Tomato, second Coke and cake no gluten with tea....
    
    Mummi orange-rice-flour cake on 20-Dec!!
    
    Mummi (tinytaura) watching and chatting contribution!!
    
    bilbo sleeping and needing/requesting/crying for help care (for the right person (me), the cats wanna fight with him) contribution!!
    
    camandcode (Cameron White) streaming & friendship contribution!!
    
    boflux (Spoofh, Benjamin Kuhl, from Germany) chatting contribution!!
    
    lowluds (from Canada) chatting and general support contribution!!
    
    smokesess (from Canada) chatting and general support contribution!!
    
    shegotmad (at London) chatting, learning C++ and talking about ca2 contribution!!
    
    jusg_fpga (fpga_guru, vue_equalizer, just_fpga, Oliver Pohl) chatting contribution!!
    
    tomrandall streaming Ludum contribution!!
    
    alfredotigolo and Okbatgames chatting contribution!!
    
    xmetrix streaming contribution!!
    
    JessicaMak and marcobrunodev streaming contribution!!
    
    whyyyyyyysoserious streaming chess contribution!!
    
    fpga_guru (vue_equalizer, Oliver from Deutsch) C++/ca2 interest contribution!!
    
    Myrkee (Valheim) streaming contribution!!
    
    EinfachUwe42 streaming contribution!!
    
    JPCdk streaming contribution!!
    
    sodapoppin and friends contribution!!
    
    cmgriffing streaming contribution!!
    
    TimBeaudet (Friends: FletcherLabs, tsjost and Jabokoe) streaming contribution!!
    
    sodapoppin streaming contribution!!
    
    Roxkstar74 sleeping streaming contribution!!
    
    kissloryshy chatting contribution!!
    
    jmcmorris (Jason Morris, SiegeGames) streaming contribution!!
    
    Naysayer88 (j_blow) and friends contribution!!
    
    PocketWatch (Joseph Riedel 'nd Andy Schatz) streaming C++ contribution!!
    
    strager streaming C++ contribution!!
    
    JessicaMak streaming contribution!!
    
    WTSRetro/SpiffyDane and Myrkee streaming contribution!!
    
    lastmiles streaming farwest contribution!!
    
    zeeth (and Karlos and friends from Danmark) streaming contribution!!
    
    deniz_1418 (and friends fra Danmark) streaming contribution!!
    
    rwxrob streaming contribution!!
    
    rw_grim streaming contribution!!
    
    phanxgames (Henri Price) streaming contribution!!
    
    PandemicMoon (Dunno, P4ndaExpress, Andreas Pedersen from Denmark) chatting and streaming contribution!!
    
    Zorchenhimer streaming contribution!!
    
    diegobrando_linux (Bl4ck_gookoo) chatting contribution!!
    
    jhovgaard streaming contribution!!
    
    Klay4_ chatting contribution!!
    
    HonestDanGames streaming contribution!!
    
    NorthSeaHero streaming contribution!!
    
    GexYT streaming contribution!!
    
    PirateSoftware streaming contribution!!
    
    EvanMMO streaming contribution!!
    
    OfficialAndyPyro streaming contribution!!
    
    Primeagen streaming contribution!!
    
    Jaxstyle and friends streaming contribution!!
    
    Melchizedek6809 and btcfly streaming contribution!!
    
    hvalen_hvalborg112 streaming contribution!!
    
    SomewhatAccurate C++ streaming contribution!!
    
    VerFX streaming contribution!!
    
    ValashLev6@twitch (from Russia) ca2 viewership and coding collaboration!!
    
    Listening to Bryan Adams, Westlife, Shayne Ward, MLTR, Backstreet Boys, Boyzone - Best Love Songs Ever by Relax Song at YouTube!!
    
    -- hi5 contribution...!!
    
    macOS Tahoe 26.3.1 at fox@ca2 remotely from Windows 10 Pro!!
    Mac mini M4 16GB 256GB SSD External SATA 480GB 1TB
    Jeg Elsker Dig Thomas Borregaard Sørensen!!
    
    Signed-off-by: Camilo Sasuke Thomas Borregaard Sørensen <camilosasuketbs@gmail.com>

---
+++ remoting_macos/desktop/CopyRectDetector.cpp
@@ -0,0 +1,118 @@
+// Copyright (C) 2008,2009,2010,2011,2012 GlavSoft LLC.
+// All rights reserved.
+//
+//-------------------------------------------------------------------------
+// This file is part of the TightVNC software.  Please visit our Web site:
+//
+//                       http://www.tightvnc.com/
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, w_rite to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//-------------------------------------------------------------------------
+//
+#include "framework.h"
+#include "CopyRectDetector.h"
+#include "subsystem/_common_header.h"
+#include "acme/_operating_system.h"
+
+namespace remoting
+{
+
+   CopyRectDetector::CopyRectDetector() {}
+
+   CopyRectDetector::~CopyRectDetector() {}
+
+
+   // Callback routine used internally to catch window movement...
+   static bool CALLBACK enumWindowsFnCopyRect(HWND hwnd, LPARAM lparam)
+   {
+      auto _this = (CopyRectDetector *) lparam;
+      return _this->checkWindowMovements(::as_operating_system_window(hwnd));
+   }
+
+
+   void CopyRectDetector::detectWindowMovements(::i32_rectangle &rectangleCopy, ::i32_point & pointSource)
+   {
+      m_rectangleCopy.clear();
+      m_pointSource.clear();
+
+      EnumWindows((WNDENUMPROC)enumWindowsFnCopyRect, (::lparam)(void *) this);
+      m_lastWinProps = m_newWinProps;
+      m_newWinProps.clear();
+      rectangleCopy = m_rectangleCopy;
+      pointSource = m_pointSource;
+   }
+
+
+   bool CopyRectDetector::checkWindowMovements(const ::operating_system::window & operatingsystemwindow)
+   {
+      ::i32_rectangle rectangleCurrent;
+      //auto hwnd = ::as_HWND(operatingsystemwindow);
+      if (IsWindowVisible(::as_HWND(operatingsystemwindow)) && getWinRect(operatingsystemwindow, rectangleCurrent))
+      {
+         // Store window properties in the new ::list_base
+         WinProp newWinProp(operatingsystemwindow, rectangleCurrent);
+         m_newWinProps.add(newWinProp);
+
+         ::i32_rectangle rectangleOld;
+         if (findPrevWinProps(operatingsystemwindow, rectangleOld))
+         {
+            if (rectangleOld.top_left() != rectangleCurrent.top_left() && rectangleCurrent.area() > m_rectangleCopy.area())
+            {
+
+               m_rectangleCopy = rectangleCurrent;
+
+               m_pointSource = rectangleOld.top_left();
+
+               // Adjust
+               i32_size sizeDesktop(GetSystemMetrics(SM_XVIRTUALSCREEN),GetSystemMetrics(SM_YVIRTUALSCREEN));
+
+               m_rectangleCopy -= sizeDesktop;
+
+               m_pointSource -= sizeDesktop;
+
+            }
+         }
+      }
+      return true;
+   }
+
+   bool CopyRectDetector::getWinRect(const ::operating_system::window & operatingsystemwindow, ::i32_rectangle & rectangle)
+   {
+      return ::windows::get_window_rect(operatingsystemwindow, rectangle);
+   }
+
+   bool CopyRectDetector::findPrevWinProps(const ::operating_system::window & operatingsystemwindow, ::i32_rectangle & rectangle)
+   {
+      //::list_base<WinProp>::iterator winPropsIter;
+      //WinProp *winProp;
+      for (auto winprop:m_lastWinProps)
+      {
+        // winProp = &(*winPropsIter);
+         if (winprop.m_operatingsystemwindow == operatingsystemwindow)
+         {
+            rectangle = winprop.m_rectangleOld;
+            return true;
+         }
+      }
+
+      return false;
+   }
+
+
+} // namespace remoting
+
+
+
+
